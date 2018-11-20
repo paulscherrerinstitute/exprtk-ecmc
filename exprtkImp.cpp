1e3f9f25fe216a0df19fb33cb192f43b8a6d96c1
Anders Sandstrom <anders.sandstrom@esss.se>
2018-11-20 12:04:07 +0100

    Add support for vectors, constants, string variables and access to exprtk fileIO lib.

--- exprtkImp.cpp
+++ exprtkImp.cpp
@@ -7,6 +7,28 @@
 
 #include "exprtkImp.h"
 
+#define generate_add_function_imp_cpp(NN)                                                \
+int exprtkImp::addFunction(const std::string& functionName, ff##NN##_functor function)   \
+{                                                                                        \
+  return !symbolTable_.add_function(functionName,function);                              \
+}                                
+
+generate_add_function_imp_cpp(00)
+generate_add_function_imp_cpp(01)
+generate_add_function_imp_cpp(02)
+generate_add_function_imp_cpp(03)
+generate_add_function_imp_cpp(04)
+generate_add_function_imp_cpp(05)
+generate_add_function_imp_cpp(06)
+generate_add_function_imp_cpp(07)
+generate_add_function_imp_cpp(08)
+generate_add_function_imp_cpp(09)
+generate_add_function_imp_cpp(10)
+generate_add_function_imp_cpp(11)
+generate_add_function_imp_cpp(12)
+generate_add_function_imp_cpp(13)
+generate_add_function_imp_cpp(14)
+generate_add_function_imp_cpp(15)
 
 exprtkImp::exprtkImp ()
 {
@@ -36,4 +58,35 @@ void exprtkImp::refresh(void)
 std::string exprtkImp::getParserError()
 {
   return parser_.error();
+}
+
+int exprtkImp::addVector(const std::string& vectorName, double* v, const std::size_t& size)
+{
+  return  !symbolTable_.add_vector(vectorName,v,size);
+}
+
+int exprtkImp::addConstants()
+{
+  return  !symbolTable_.add_constants();
+}
+
+int exprtkImp::addConstant(const std::string& constantName,double& d)
+{
+  return !symbolTable_.add_constant(constantName,d);
+}
+
+int exprtkImp::addStringvar(const std::string& stringvarName, std::string& s, const bool isConstant)
+{
+  return !symbolTable_.add_stringvar(stringvarName,s,isConstant);
+}
+
+int exprtkImp::addFileIO()
+{
+  if(!symbolTable_.add_function("println",println_)){
+    return 1;
+  }
+  if(!symbolTable_.add_package (fileio_package_)){
+    return 2;
+  }
+  return 0;
 }
--- exprtkImp.h
+++ exprtkImp.h
@@ -16,12 +16,36 @@ typedef exprtk::symbol_table<double> symbol_table_double;
 typedef exprtk::expression<double>   expression_double;
 typedef exprtk::parser<double>       parser_double;
 
+#define generate_add_function_imp_h(NN)                                       \
+int addFunction(const std::string& function_name, ff##NN##_functor function); \
+
 class exprtkImp: public exprtkIF
 {
 public:
   exprtkImp ();
   virtual ~exprtkImp ();
-  int addVariable(const std::string& variable_name, double& d);
+  int addVariable(const std::string& variableName, double& d);
+  generate_add_function_imp_h(00)
+  generate_add_function_imp_h(01)
+  generate_add_function_imp_h(02)
+  generate_add_function_imp_h(03)
+  generate_add_function_imp_h(04)
+  generate_add_function_imp_h(05)
+  generate_add_function_imp_h(06)
+  generate_add_function_imp_h(07)
+  generate_add_function_imp_h(08)
+  generate_add_function_imp_h(09)
+  generate_add_function_imp_h(10)
+  generate_add_function_imp_h(11)
+  generate_add_function_imp_h(12)
+  generate_add_function_imp_h(13)
+  generate_add_function_imp_h(14)
+  generate_add_function_imp_h(15)
+  int addVector(const std::string& vectorName, double* v, const std::size_t& size);
+  int addConstants();
+  int addConstant(const std::string& constantName,double& d);
+  int addStringvar(const std::string& stringvarName, std::string& s, const bool isConstant);
+  int addFileIO();
   int compile(std::string expressionString);
   void refresh(void);
   std::string getParserError();
@@ -29,6 +53,8 @@ private:
   symbol_table_double symbolTable_;
   expression_double expression_;
   parser_double parser_;
+  exprtk::rtl::io::file::package<double> fileio_package_;
+  exprtk::rtl::io::println<double>       println_;
 };
 
 #endif /* M_EPICS_EXPRTK_EXPRTKIMP_H_ */
